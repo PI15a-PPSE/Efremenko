70540b43d984f322654ff9d40883f6ad3fe184c7
GlebEfrem <gleb.knyzev@mail.ru>
2018-11-22 01:08:48 +0300

    Finishing up JS code

--- script.js
+++ script.js
@@ -139,4 +139,127 @@ window.onload = function() {
             }
         };
     }
+    
+    //Board object - controls logistics of game
+    var Board = {
+        board: gameBoard,
+        playerTurn: 1,
+        tilesElement: $('div.tiles'),
+        //dictionary to convert position in Board.board to the viewport units
+        dictionary: ["0vmin", "10vmin", "20vmin", "30vmin", "40vmin", "50vmin", "60vmin", "70vmin", "80vmin", "90vmin"],
+        //initialize the 8x8 board
+        initalize: function () {
+            var countPieces = 0;
+            var countTiles = 0;
+            for (row in this.board) { //row is the index
+                for (column in this.board[row]) { //column is the index
+                    //whole set of if statements control where the tiles and pieces should be placed on the board
+                    if(row%2 == 1) {
+                        if(column%2 == 0) {
+                            this.tilesElement.append("<div class='tile' id='tile"+countTiles+"' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
+                            tiles[countTiles] = new Tile($("#tile"+countTiles), [parseInt(row), parseInt(column)]);
+                            countTiles += 1;
+                        }
+                    } else {
+                        if(column%2 == 1) {
+                            this.tilesElement.append("<div class='tile' id='tile"+countTiles+"' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
+                            tiles[countTiles] = new Tile($("#tile"+countTiles), [parseInt(row), parseInt(column)]);
+                            countTiles += 1;
+                        }
+                    }
+                    if(this.board[row][column] == 1) {
+                        $('.player1pieces').append("<div class='piece' id='"+countPieces+"' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
+                        pieces[countPieces] = new Piece($("#"+countPieces), [parseInt(row), parseInt(column)]);
+                        countPieces += 1;
+                    } else if(this.board[row][column] == 2) {
+                        $('.player2pieces').append("<div class='piece' id='"+countPieces+"' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
+                        pieces[countPieces] = new Piece($("#"+countPieces), [parseInt(row), parseInt(column)]);
+                        countPieces += 1;
+                    }
+                }
+            }
+        },
+        //check if the location has an object
+        isValidPlacetoMove: function (row, column) {
+            console.log(row); console.log(column); console.log(this.board);
+            if(this.board[row][column] == 0) {
+                return true;
+            } return false;
+        },
+        //change the active player - also changes div.turn's CSS
+        changePlayerTurn: function () {
+            if(this.playerTurn == 1) {
+                this.playerTurn = 2;
+                $('.turn').css("background", "linear-gradient(to right, transparent 50%, #BEEE62 50%)");
+                return;
+            }
+            if(this.playerTurn == 2) {
+                this.playerTurn = 1;
+                $('.turn').css("background", "linear-gradient(to right, #BEEE62 50%, transparent 50%)");
+            }
+        },
+        //reset the game
+        clear: function () {
+            location.reload(); 
+        }
+    }
+    
+    //initialize the board
+    Board.initalize();
+    
+    /***
+    Events
+    ***/
+    
+    //select the piece on click if it is the player's turn
+    $('.piece').on("click", function () {
+        var selected;
+        var isPlayersTurn = ($(this).parent().attr("class").split(' ')[0] == "player"+Board.playerTurn+"pieces");
+        if(isPlayersTurn) {
+            if($(this).hasClass('selected')) selected = true;
+            $('.piece').each(function(index) {$('.piece').eq(index).removeClass('selected')});
+            if(!selected) {
+                $(this).addClass('selected');
+            }
+        }
+    });
+    
+    //reset game when clear button is pressed
+    $('#cleargame').on("click", function () {
+        Board.clear();
+    });
+    
+    //move piece when tile is clicked
+    $('.tile').on("click", function () {
+        //make sure a piece is selected
+        if($('.selected').length != 0) {
+            //find the tile object being clicked
+            var tileID = $(this).attr("id").replace(/tile/, '');
+            var tile = tiles[tileID];
+            //find the piece being selected
+            var piece = pieces[$('.selected').attr("id")];
+            //check if the tile is in range from the object
+            var inRange = tile.inRange(piece);
+            if(inRange) {
+                //if the move needed is jump, then move it but also check if another move can be made (double and triple jumps)
+                if(inRange == 'jump') {
+                    if(piece.opponentJump(tile)) {
+                        piece.move(tile);
+                        if(piece.canJumpAny()) {
+                             Board.changePlayerTurn(); //change back to original since another turn can be made
+                             piece.element.addClass('selected');
+                        }
+                    } 
+                    //if it's regular then move it if no jumping is available
+                } else if(inRange == 'regular') {
+                    if(!piece.canJumpAny()) {
+                        piece.move(tile);
+                    } else {
+                        alert("You must jump when possible!");
+                    }
+                }
+            }
+        }
+    });
+    
 }
