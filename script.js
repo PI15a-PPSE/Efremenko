46c21a5e3a1732119826a0bc16b847ddf6bc6ab2
GlebEfrem <gleb.knyzev@mail.ru>
2018-11-22 00:42:39 +0300

    Created script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function() {    
+    //The initial setup
+    var gameBoard = [ 
+        [  0,  1,  0,  1,  0,  1,  0,  1 ],
+        [  1,  0,  1,  0,  1,  0,  1,  0 ],
+        [  0,  1,  0,  1,  0,  1,  0,  1 ],
+        [  0,  0,  0,  0,  0,  0,  0,  0 ],
+        [  0,  0,  0,  0,  0,  0,  0,  0 ],
+        [  2,  0,  2,  0,  2,  0,  2,  0 ],
+        [  0,  2,  0,  2,  0,  2,  0,  2 ],
+        [  2,  0,  2,  0,  2,  0,  2,  0 ]
+    ];
+    //arrays to store the instances
+    var pieces = [];
+    var tiles = []; 
+}
